83ef8b80ddf5e8eaf455dba65501718152da50e6
choonkeun <choonkeun@yahoo.com>
2018-03-07 06:39:20 -0800

    upload

--- MonitorFolder1.1/FileListener.sql
+++ MonitorFolder1.1/FileListener.sql
@@ -2,6 +2,8 @@
 USE [FileListener]
 GO
 
+DROP TABLE Action
+GO
 CREATE TABLE [dbo].[Action](
 	[Id]            [tinyint] NULL,
 	[Name]          [varchar](50) NULL
@@ -10,41 +12,60 @@ GO
 
 INSERT [dbo].[Action] ([Id], [Name]) VALUES (0, N'None')
 INSERT [dbo].[Action] ([Id], [Name]) VALUES (1, N'Copy')
-INSERT [dbo].[Action] ([Id], [Name]) VALUES (2, N'Move')
+INSERT [dbo].[Action] ([Id], [Name]) VALUES (2, N'Move/Rename')
 INSERT [dbo].[Action] ([Id], [Name]) VALUES (3, N'Delete')
 INSERT [dbo].[Action] ([Id], [Name]) VALUES (4, N'Zip')
+INSERT [dbo].[Action] ([Id], [Name]) VALUES (6, N'Zip Password')
 INSERT [dbo].[Action] ([Id], [Name]) VALUES (5, N'UnZip')
-INSERT [dbo].[Action] ([Id], [Name]) VALUES (6, N'Zip with Password')
-INSERT [dbo].[Action] ([Id], [Name]) VALUES (7, N'UnZip with Password')
-INSERT [dbo].[Action] ([Id], [Name]) VALUES (8, N'Encrypt with Password')
-INSERT [dbo].[Action] ([Id], [Name]) VALUES (9, N'Decrypt with Password')
+INSERT [dbo].[Action] ([Id], [Name]) VALUES (7, N'UnZip Password')
+INSERT [dbo].[Action] ([Id], [Name]) VALUES (8, N'Encrypt')
+INSERT [dbo].[Action] ([Id], [Name]) VALUES (9, N'Decrypt')
+INSERT [dbo].[Action] ([Id], [Name]) VALUES (10, N'FTP Upload')
+INSERT [dbo].[Action] ([Id], [Name]) VALUES (11, N'FTP Download')
+INSERT [dbo].[Action] ([Id], [Name]) VALUES (12, N'FTP Move/Rename')
+
+SELECT * FROM Action
 
 Id   Name
 ---- --------------------------------------------------
 0    None
 1    Copy
-2    Move/Change
+2    Move/Rename
 3    Delete
 4    Zip
+6    Zip Password
 5    UnZip
-6    Zip with Password
-7    UnZip with Password
-8    Encrypt with Password
-9    Decrypt with Password
+7    UnZip Password
+8    Encrypt
+9    Decrypt
+10   FTP Upload
+11   FTP Download
+12   FTP Move/Rename
 
 
 /****** Object:  Table [dbo].[WatchTask]    Script Date: 3/5/2018 7:35:43 PM ******/
+DROP TABLE WatchTask
+GO
 CREATE TABLE [dbo].[WatchTask](
 	[Id]                [int] NOT NULL,
 	[Name]              [varchar](50) NULL,
-	[Sequence]          [tinyint] NULL,
+	[ProcessId]         [int] NULL,
 	[JobId]             [int] NULL,
-	[WatchPattern]      [varchar](100) NULL,
-	[ActionId]          [tinyint] NULL,
-	[IsMultiple]        [bit] NULL,
+	[Sequence]          [tinyint] NULL,
+	[WatchPattern]      [varchar](1000) NULL,
+	[isAND]             [bit] NULL,             --만일 true이면 WatchPattern에 나열된 Pattern이 모두 AND조건을 충족해야 한다. 
+	[ActionId]          [tinyint] NULL,         --예를들어 "777*.txt, 888*.out, 999.txt"의 경우 3가지 Patter의 File이 모두 있어야 실행이 된다.
+	[isSourceFTP]       [bit] NULL,             --isAND=False이면 OR 조건이므로 Pattern 하나만 만족해도 실행이 된다.
+	[SourceFTPId]       [smallint] NULL,        --Source FTP login information
+	[SourcePath]        [varchar](100) NULL,    
+	[SourceFileName]    [varbinary](50) NULL,   
+	[SourceFileExt]     [varchar](50) NULL,
+	[isTargetFTP]       [bit] NULL,
+	[TargetFTPId]       [smallint] NULL,        --Target FTP login information
 	[TargetPath]        [varchar](100) NULL,
 	[TargetFileName]    [varbinary](50) NULL,
-	[TargetFileExtension] [varchar](50) NULL,
+	[TargetFileExt]     [varchar](50) NULL,
+	[Password]          [varchar](50) NULL,     --Zip/UnZip Password, Encrypt/Decrypt Password
 	[EMail]             [varchar](50) NULL,
 	[EMailGroupId]      [smallint] NULL,
 	[StatusNotify]      [bit] NULL,
@@ -53,15 +74,18 @@ CREATE TABLE [dbo].[WatchTask](
 (	[Id] ASC    )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
+SELECT * FROM WatchTask
 
 /****** Object:  Table [dbo].[WatchJob]    Script Date: 3/5/2018 7:35:43 PM ******/
+DROP TABLE WatchJob
+GO
 CREATE TABLE [dbo].[WatchJob](
 	[Id]                [int] IDENTITY(1,1) NOT NULL,
 	[Name]              [varchar](50) NULL,
-	[Sequence]          [tinyint] NULL,
 	[ProcessId]         [int] NULL,
+	[Sequence]          [tinyint] NULL,
 	[GroupName]         [varchar](50) NULL,
-	[GroupCondition]    [bit] NOT NULL,
+	[GroupCondition]    [bit] NOT NULL,     
 	[EMail]             [varchar](50) NULL,
 	[EMailGroupId]      [smallint] NULL,
 	[StatusNotify]      [bit] NULL,
@@ -84,12 +108,14 @@ CREATE TABLE [dbo].[WatchProcess](
 
 
 /****** Object:  Table [dbo].[WatchOwner]    Script Date: 3/5/2018 7:35:43 PM ******/
+DROP TABLE WatchOwner
+GO
 CREATE TABLE [dbo].[WatchOwner](
 	[Id]                [int] NOT NULL,
 	[Name]              [varchar](50) NULL,
 	[Description]       [varchar](100) NULL,
-	[Grade]             [tinyint] NULL,         0:Administrator, 1:SuperUser, 
-	[Group]             [varchar](50) NULL,
+	[Grade]             [tinyint] NULL,         --0:Administrator, 1:SuperUser, 
+	[OwnerGroup]        [varchar](50) NULL,
  CONSTRAINT [PK_WatchOwner] PRIMARY KEY CLUSTERED 
 (	[Id] ASC )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
@@ -154,6 +180,20 @@ CREATE TABLE [dbo].[EMailGroupList](
 (	[EMailGroupId] ASC,
 	[EMailAddressId] ASC
 ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[FTPSite]    Script Date: 3/5/2018 11:17:06 PM ******/
+CREATE TABLE [dbo].[FTPSite] (
+	[Id]                [int] NOT NULL,
+	[Name]              [varchar](50) NULL,
+	[URL]               [varchar](50) NULL,
+	[UserName]          [varchar](50) NULL,
+	[Password]          [varchar](50) NULL,
+	[Protocol]          [varchar](10) NULL,
+	[Port]              [smallint] NULL,
+ CONSTRAINT [PK_FTPSite] PRIMARY KEY CLUSTERED 
+(	[Id] ASC ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
